cc6c1ceeea43c3ebeeae00eb7c701288b6a828d9
nour <nourslama13@gmail.com>
2020-04-06 19:45:13 +0200

    secondcommit

--- src/components/rating.js
+++ src/components/rating.js
@@ -7,7 +7,7 @@ export default function rating(props) {
         if(i<=rate)
        starArray.push (<i key={i} className="fas fa-star star-gold" onClick={() => onChangeRating(i)} ></i>)
    else
-        starArray.push (<i key={i} className="far fa-star" onClick={() => onChangeRating(i)}></i>)
+        starArray.push (<i key={i} className="far fa-star" onClick={() => onChangeRating(i-1)}></i>)
  }
     
     return (
